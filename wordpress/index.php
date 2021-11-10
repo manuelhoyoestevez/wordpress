df6ba4ae9b09b8f344ed4a2ae5cb29130d8ee21c
Manuel Hoyo Estévez <manuel.hoyo@inetum.world>
2021-11-10 22:51:28 +0100

    Wordpress without installing

--- wordpress/index.php
+++ wordpress/index.php
@@ -1,10 +1,17 @@
 <?php
-phpinfo();
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-echo "PHP " . phpversion() . " working!";
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-for ($i = 0; $i < 10; $i++) {
-    echo "\$i = $i<br/>\n";
-}
-
-echo "Finished!";
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
